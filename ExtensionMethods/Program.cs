426512fe7ebff5beaa2a5a79d972f09947489aef
mohammedelmoghazi <mohammed.elmoghazi@alareebict.com>
2024-07-22 22:24:34 +0300

    ExtensionMethods

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ExtensionMethods
+{
+
+    /// <summary>
+    /// Extension Methods: Allow us to add methods to existing class without:
+    /// - Changing its source code, or 
+    /// - Creating a new class that inherit from it.
+    /// Create the extension methods if you do not have the source code of the class.
+    /// Most of the time you will be using the existing extension methods.
+    /// Extension Method creates as a static member in a static class with this param.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+
+            string post = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+            string shortenPost = post.Shorten(11);
+            Console.WriteLine("Extension Method Shorten():" + shortenPost);
+
+            //Exmaple of existing extension method
+            IEnumerable<int> numbers = new List<int> { 1,2,3,4,5};
+            numbers.Max();
+
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace ExtensionMethods
+{
+    //public class StringExtensions : String
+    //{
+        // can not inherit from sealed class
+    //}
+
+    public static class StringExtensions
+    {
+        public static string Shorten(this string str, int numberOfCharacters)
+        {
+            return str.Substring(0, numberOfCharacters);
+        }
+    }
+}
