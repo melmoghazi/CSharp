679c6dac22442fabb3df73d7fc1e6d2c15516848
mohammedelmoghazi <mohammed.elmoghazi@alareebict.com>
2024-07-25 17:30:30 +0300

    DemoExtensionMethods and DemoNullableTypes

---
+++ DemoDynamics/Program.cs
@@ -0,0 +1,46 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+namespace DemoDynamics
+{
+    internal class Program
+    {
+        /// <summary>
+        /// programming languages:
+        ///     - Statically-typed languages: C#, Java.
+        ///         - type resolution is at compile time.
+        ///         - benefit: early feedback (at compile time)
+        ///     - Dynamically-typed languages: Javascript, python, ruby.
+        ///         - type resolution is at run time.
+        ///         - benefit: easer and faster to code. 
+        ///  ----------------------------------------------------------------------------------------       
+        /// 
+        /// Reflection is a way to inspect a metadata about a type and access properities and methods.
+        /// -----------------------------------------------------------------------------------------
+        /// It is a bad practice to use dynamic.
+        /// because dynamic keyword refers to type late binding, which means the system will check type only during execution instead of during compilation.
+        ///     It will then mean that user, instead of programmer, is left to discover the potential error.
+        /// To solve the previous point you could rely on unit tests, non regression human tests and so on but it can lead to increased maintenance costs.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            Console.WriteLine();
+
+            var name = "Mohammed";
+            Console.WriteLine(name);
+            //name = 10;//cannot implicitly convert type int to string.
+
+            dynamic myName = "Mohammed";
+            Console.WriteLine(myName);
+            myName = 10;
+            Console.WriteLine(myName);
+
+
+            Console.WriteLine();
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NullableTypes/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace NullableTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            Console.WriteLine();
+            //can not convert null to a datetime becuase it is a non-nullable value type
+            //DateTime date = null;
+
+            Nullable<DateTime> date = null;
+            Console.WriteLine($"date= {date}");
+
+            //short hand of nullable value type
+            DateTime? birthDate = null;
+            Console.WriteLine($"birthDate HasValue= {birthDate.HasValue}");
+            Console.WriteLine($"birthDate GetValueOrDefault= {birthDate.GetValueOrDefault()}");
+            //The following will throw exception so you should check birthDate.HasValue first.
+            //Console.WriteLine($"birthDate Value= {birthDate.Value}");
+
+            //DateTime date2 = birthDate;
+            DateTime date2 = birthDate.GetValueOrDefault();
+            Console.WriteLine($"date2 ={date2}");
+
+            //null coalescing operator
+            DateTime date3 = date ?? birthDate.GetValueOrDefault();
+
+            //ternary conditional operator
+            DateTime date4 = (date != null) ? birthDate.GetValueOrDefault() : DateTime.Now;
+
+
+            Console.WriteLine();
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+    }
+}
