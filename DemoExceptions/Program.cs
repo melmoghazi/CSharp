69d0e21841d7695bb67dc28542a5227867874823
mohammedelmoghazi <mohammed.elmoghazi@alareebict.com>
2024-08-04 17:20:17 +0300

    DemoExceptions

---
+++ DemoExceptions/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace DemoExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            YouTubeAPI youTubeAPI = new YouTubeAPI();
+            youTubeAPI.GetVideos("Ali");
+            Console.WriteLine("--------------------------------------");
+            ReadFile();
+            Console.WriteLine("--------------------------------------");
+
+            try
+            {
+                int result = Calculate(1, 0);
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by Zero.");
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("ArithmeticException");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+
+            }
+
+
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+
+        static int Calculate(int numerator, int denominator)
+        {
+            return (numerator / denominator);
+        }
+
+        static void ReadFile()
+        {
+            StreamReader streamReader = null;
+            try
+            {
+                streamReader = new StreamReader(@"c:\file.zip");
+                var content = streamReader.ReadToEnd();
+                throw new Exception("Oops");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sorry, unexpected error happen while reading the file.");
+            }
+            finally
+            {
+                if (streamReader != null)
+                    streamReader.Dispose();
+            }
+        }
+        static void ReadFile_Using()
+        {
+            try
+            {
+                //using will create under the hood the finally block and call Dispose method.
+                using (StreamReader streamReader = new StreamReader(@"c:\file.zip"))
+                {
+                    var content = streamReader.ReadToEnd();
+                    throw new Exception("Oops");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sorry, unexpected error happen while reading the file.");
+            }
+
+        }
+    }
+}
---
+++ DemoExceptions/YouTube.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoExceptions
+{
+    public class YouTubeException:Exception
+    {
+        public YouTubeException(string message, Exception innerException)
+            :base(message, innerException)
+        {
+                
+        }
+    }
+    public class YouTubeAPI
+    {
+        public List<Video> GetVideos(string user)
+        {
+            try
+            {
+                //access youtube web service
+                //read the data
+                //create a list of video objects
+                throw new Exception("Oops");
+                return new List<Video>();
+            }
+            catch (Exception ex )
+            {
+                //log
+                throw new YouTubeException("Could not fetch the videos from youtube.", ex);
+            }
+        }            
+    }
+    public class Video
+    {
+    }
+}
