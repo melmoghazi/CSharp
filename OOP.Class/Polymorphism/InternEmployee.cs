2c71a76592577bce1f4ec017b5c1e67593404c4f
mohammedelmoghazi <mohammed.elmoghazi@alareebict.com>
2024-08-20 15:45:41 +0300

    OOP

---
+++ OOP.Class/Polymorphism/InternEmployee.cs
@@ -0,0 +1,11 @@
+﻿namespace OOP.Class.Polymorphism
+{
+    internal class InternEmployee : Employee
+    {
+        public override decimal GetSalary()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            return 3000;
+        }
+    }
+}
